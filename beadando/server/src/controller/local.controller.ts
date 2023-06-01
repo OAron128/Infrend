db194374d29363c433547fe5b7e0ff4e6bd89c87
OAron <79246698+OAron128@users.noreply.github.com>
2023-06-01 09:25:18 +0200

    Delete beadando directory

--- beadando/server/src/controller/local.controller.ts
+++
@@ -1,7 +0,0 @@
-import { AppDataSource } from "../data-source";
-import { Local } from "../entity/Local";
-import { Controller } from "./base.controller";
-
-export class LocalController extends Controller{
-    repository = AppDataSource.getRepository(Local)
-}
